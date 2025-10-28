f1aad30a6362573540366a19b696c12a849240ca
jean <741jean@gmail.com>
2025-10-28 10:42:54 -0300

    Update questao4.cpp

--- listas/lista2/q4/questao4.cpp
+++ listas/lista2/q4/questao4.cpp
@@ -5,24 +5,25 @@ using namespace std;
 int main()
 {
 	//entrada
-	string na1,na2,conteudo,conteudo2;
+	string nome1,nome2,nome3,conteudo,conteudo2;
 	cin>>na1>>na2;
 	
 	//primeiro arquivo
-	ifstream arq1(na1);
+	ifstream arq1(nome1);
 	arq1>>conteudo;
 	arq1.close();
 	
 	//segundo arquivo
-	ifstream arq2(na2);
+	ifstream arq2(nome2);
 	arq2>>conteudo2;
 	arq2.close();
 	
 	//terceiro arquivo
-	ofstream arq3("arquivo3.txt");
+	ofstream arq3(nome3);
 	arq3<<conteudo<<" "<<conteudo2;
 	arq3.close();
 	
 	return 0;
 	
 }
+
